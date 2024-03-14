5b5affa97d8e3d49441855ce984a78bc49db57a4
อัษฎาวุธ ไชยรักษ์ <yakukung@MacBookAir-2.local>
2024-03-14 21:34:56 +0700

    Update all files

--- src/app/components/edit-profile/edit-profile.component.ts
+++ src/app/components/edit-profile/edit-profile.component.ts
@@ -44,7 +44,7 @@ export class EditProfileComponent {
   }
 
   fetchUserData(user_id: string) {
-    const url = `http://localhost:3000/facemash/navbar`;
+    const url = `https://project-backend-2-2.onrender.com/facemash/navbar`;
 
     this.httpClient.post(url, { user_id }).subscribe(
       (response: any) => {
@@ -68,7 +68,7 @@ export class EditProfileComponent {
     );
   }
   fetchPostData(user_id: string) {
-    const postUrl = `http://localhost:3000/facemash/profile`;
+    const postUrl = `https://project-backend-2-2.onrender.com/facemash/profile`;
   
     this.httpClient.post(postUrl, { user_id })
       .subscribe(
@@ -128,8 +128,8 @@ export class EditProfileComponent {
       const formData = new FormData();
       formData.append('file', selectedFile);
   
-      // Replace 'http://localhost:3000' with your actual server URL
-      const uploadUrl = `http://localhost:3000/upload/${user_id}/${post_id}`;
+      // Replace 'https://project-backend-2-2.onrender.com' with your actual server URL
+      const uploadUrl = `https://project-backend-2-2.onrender.com/upload/${user_id}/${post_id}`;
   
       this.http.post(uploadUrl, formData).subscribe(
         (response: any) => {
